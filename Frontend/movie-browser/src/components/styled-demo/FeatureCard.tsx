c41a9547df8c4cfa931e5034f1bbb6178ddf08dd
TimoSR <timothy.s.rasmussen@gmail.com>
2025-11-26 14:37:05 +0100

    Type import example and adding tailwind support through styles

--- Frontend/movie-browser/src/components/styled-demo/FeatureCard.tsx
+++ Frontend/movie-browser/src/components/styled-demo/FeatureCard.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import styled from 'styled-components'
 
 export const FeatureCard = ({ title, description, icon, color, className = '' }: FeatureCardProps) => {
